1b26264e9e4f225301347263f50246994252f946
Tom <tom.mora96@gmail.com>
2018-06-27 17:55:58 -0300

    Mas lindos los comments de Vector.h

--- ADT_Vector.h
+++ ADT_Vector.h
@@ -17,23 +17,23 @@ typedef struct {
 	exporter_t csv_exporter;
 } ADT_Vector_t;
 
-/*Constructors*/
+/*-------------------Constructors-------------------*/
 status_t ADT_Vector_new (ADT_Vector_t ** pp);
 status_t ADT_Vector_expand (ADT_Vector_t * v, size_t chop_size);
 
-/*Destructors*/
+/*-------------------Destructors--------------------*/
 status_t ADT_Vector_delete (ADT_Vector_t ** pp);
 status_t ADT_Vector_set_destructor (ADT_Vector_t * v, destructor_t pf);
 
-/*Getters*/
+/*-------------------Getters------------------------*/
 size_t ADT_Vector_get_size(const ADT_Vector_t *pp);
 void * ADT_Vector_get_element(const ADT_Vector_t * v, size_t position);
 bool_t ADT_Vector_is_empty(const ADT_Vector_t * v);
 
-/*Adder*/
+/*-------------------Adder-------------------------*/
 status_t ADT_Vector_add_element(ADT_Vector_t * v, void * element);
 
-/*Exporters*/
+/*-------------------Exporters---------------------*/
 status_t ADT_Vector_export_as_csv(const void * v, const void * context, FILE * fo);
 status_t ADT_Vector_set_xml_exporter(ADT_Vector_t * v, exporter_t pf);
 status_t ADT_Vector_export_as_xml(const void * v, const void * context, FILE * fo);
